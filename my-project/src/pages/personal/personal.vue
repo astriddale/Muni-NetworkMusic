cf3a6cfe14ed0817fac5ddb88beed00bceb4d627
astriddale <997941600@qq.com>
2021-08-27 20:11:14 +0800

    Muni 初始化

---
+++ my-project/src/pages/personal/personal.vue
@@ -0,0 +1,14 @@
+<template>
+  <view>
+    <text>我是personal页面</text>
+  </view>
+</template>
+
+<script>
+export default {
+  name: "Personal",
+};
+</script>
+
+<style>
+</style>
---
+++ my-project/src/pages/video/video.vue
@@ -0,0 +1,16 @@
+<template>
+  <view>
+    <text>我是video页面</text>
+
+    <text>ssssss</text>
+  </view>
+</template>
+
+<script>
+export default {
+  name: "Video",
+};
+</script>
+
+<style>
+</style>
